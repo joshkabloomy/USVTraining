5919e8b89dc85679e8a286aafe7a6d835d86ebfc
Manuel Roglan <manueljoseph113@gmail.com>
2023-03-08 17:48:07 -0500

    straight line path

---
+++ training_navigation/training_navigation/planner.py
@@ -0,0 +1,72 @@
+from geometry_msgs.msg import Pose, PoseStamped
+from nav_msgs.msg import Path
+import math
+
+class Planner:
+
+    def __init__(self):
+        self.robot_pose:Pose = None
+        self.goal_pose:Pose = None
+
+    # Helper method to create a deep copy of a Pose
+    def pose_deep_copy(pose:Pose):
+        copy = Pose()
+        copy.position.x = pose.position.x
+        copy.position.y = pose.position.y
+        copy.position.z = pose.position.z
+        copy.orientation.x = pose.orientation.x
+        copy.orientation.y = pose.orientation.y
+        copy.orientation.z = pose.orientation.z
+        copy.orientation.w = pose.orientation.w
+
+        return copy
+
+class StraightPlanner(Planner):
+
+    def create_plan(self):
+        if self.robot_pose is None or self.goal_pose is None:
+            return Path()
+        
+        if self.goal_pose.position.x - self.robot_pose.position.x > 0:
+            x_dir = 1
+        else:
+            x_dir = -1
+        
+        if self.goal_pose.position.y - self.robot_pose.position.y > 0:
+            y_dir = 1
+        else:
+            y_dir = -1
+
+        path = Path()
+        path.header.frame_id = 'map'
+
+        if self.goal_pose.position.x - self.robot_pose.position.x == 0.0:
+            theta = .5 * math.pi
+        else:
+            theta = math.atan(abs(
+                (self.goal_pose.position.y - self.robot_pose.position.y) / (self.goal_pose.position.x - self.robot_pose.position.x)
+            ))
+
+        curr_pose = Planner.pose_deep_copy(self.robot_pose)
+
+        while (
+            (abs(curr_pose.position.x - self.robot_pose.position.x) 
+                < abs(self.goal_pose.position.x - self.robot_pose.position.x)) and 
+            (abs(curr_pose.position.y - self.robot_pose.position.y) 
+                < abs(self.goal_pose.position.y - self.robot_pose.position.y))
+        ):
+            path.poses.append(PoseStamped(pose=Planner.pose_deep_copy(curr_pose)))
+
+            curr_pose.position.x += math.cos(theta) * 0.2 * x_dir
+            curr_pose.position.y += math.sin(theta) * 0.2 * y_dir
+
+        path.poses.append(PoseStamped(pose=self.goal_pose))
+
+        return path
+
+
+# For you to implement!
+class CustomPlanner(Planner):
+
+    def create_plan(self):
+        pass
--- training_navigation/training_navigation/test_navigation_node.py
+++ training_navigation/training_navigation/test_navigation_node.py
@@ -1,7 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import PoseStamped, Pose
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import Odometry, Path
+from .planner import StraightPlanner, CustomPlanner
 
 class TestNavigationNode(Node):
 
@@ -18,14 +19,42 @@ class TestNavigationNode(Node):
         self.goal_sub = self.create_subscription(PoseStamped, '/goal_pose', 
             self.goal_callback, 10)
 
+        self.path_pub = self.create_publisher(Path, '/navigation/plan', 10)
+
         self.robot_pose:Pose = None
         self.goal:Pose = None
+
+        if self.get_parameter('planner').value == 'STRAIGHT_LINE':
+            self.planner = StraightPlanner()
+        else:
+            self.planner = CustomPlanner()
 
     def odom_callback(self, msg:Odometry):
         self.robot_pose = msg.pose.pose
     
     def goal_callback(self, msg:PoseStamped):
         self.goal = msg.pose
+
+        self.create_path()
+    
+    def create_path(self):
+        if self.robot_pose is None:
+            self.get_logger().info('No Robot Pose: Cannot Create Path')
+            return 
+        
+        if self.goal is None:
+            self.get_logger().info('No Goal Pose: Cannot Create Path')
+            return
+        
+        self.planner.robot_pose = self.robot_pose
+        self.planner.goal_pose = self.goal
+
+        plan = self.planner.create_plan() 
+        
+        plan.header.frame_id = 'map'
+
+        self.path_pub.publish(plan)
+
 
 def main(args=None):
     rclpy.init(args=args)
