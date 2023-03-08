a51c506f0cf39b2c4cd2fcc07b96a7f2f012f45f
Manuel Roglan <manueljoseph113@gmail.com>
2023-03-08 11:35:40 -0500

    started euclidean clustering

---
+++ training_perception/launch/main.launch.py
@@ -0,0 +1,31 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    pkg_share = get_package_share_directory('training_perception')
+
+    usv_arg = DeclareLaunchArgument('usv')
+    usv_config = LaunchConfiguration('usv')
+
+    euclidean_clustering_params_file = (pkg_share, '/config/', usv_config, 
+        '/euclidean_clustering.yaml')
+
+    return LaunchDescription([
+        usv_arg,
+
+        DeclareLaunchArgument(
+            'euclidean_clustering_params_file',
+            default_value=euclidean_clustering_params_file
+        ),
+
+        Node(
+            package='euclidean_cluster_nodes',
+            executable='euclidean_cluster_node_exe',
+            parameters=[LaunchConfiguration('euclidean_clustering_params_file')],
+            remappings=[('/points_in', '/wamv/sensors/lidars/lidar_wamv/points')]
+        )
+    ])
--- training_perception/setup.py
+++ training_perception/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'training_perception'
 
@@ -10,6 +12,14 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+
+        (os.path.join('share', package_name, 'config/simulation'), 
+            glob(os.path.join('config', 'simulation', '*.yaml'))),
+
+        (os.path.join('share', package_name, 'config/physical'),
+            glob(os.path.join('config', 'physical', '*.yaml'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
